74da92ef6a629e654e67d3029ce9afce9ac7a61a
Mi Nombre <rodrigosebastianbak@hotmail.com>
2022-05-24 22:08:08 +0200

    backend terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,6 +1,7 @@
 const {io} = require('../index');
 
-
+//Para que la conexion por Sockets funcione
+//npm install socket.io@2.3.0
 //Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
@@ -8,8 +9,24 @@ io.on('connection', client => {
         console.log('Cliente desconectado');
      });
 
+     
+
+     client.on('emitir-mensaje',(payload) => {
+         console.log(payload);
+         //Emite el mensaje a todo el mundo
+        //io.emit('nuevo-mensaje', 'Holaaa');
+
+        //Emite a todos menos al que lo emitio
+        client.broadcast.emit('nuevo-mensaje',payload);
+     });
+
+
+
+
+
+
      client.on('mensaje', (payload) =>{
-         console.log('mensaje', payload);
+        // console.log('mensaje', payload);
 
          io.emit( 'mensaje', {
             admin: `nuevo mensaje`
